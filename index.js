ef931a39ba6343de13c00fbad80c0ad1ac08a93a
adamknapp124 <aknapp124@gmail.com>
2023-03-17 19:43:39 -0700

    comments the code for ease of use in the future

--- index.js
+++ index.js
@@ -14,19 +14,24 @@ axios(url)
 		const html = response.data;
 		const $ = cheerio.load(html);
 		const items = [];
+		// TEMPLATE
 		$('.ProductItem__Wrapper, yotpo', html).each(function () {
+			// EXTRACTION EXAMPLE
+			const id = $(this).find('.ProductItem__Image').attr('data-media-id');
 			const imgUrl = $(this).find('.yotpo').attr('data-image-url');
 			const formattedUrl = imgUrl.substring(2, imgUrl.indexOf('.jpg') + 4);
-
 			const name = $(this).find('.yotpo').attr('data-name');
 			const price = $(this).find('.ProductItem__Price').text().trim();
+			// PUSH ITEMS INTO DATA ARRAY
 			items.push({
+				id,
 				category: 'hand and body skincare',
 				name,
 				price: parseFloat(price.replace('$', '')),
 				imgUrl: `https://${formattedUrl}`,
 			});
 		});
+		// CONSOLE LOG FOR ARRAY OR SEND TO DATABASE
 		console.log(items);
 	})
 	.catch((err) => console.log(err));
